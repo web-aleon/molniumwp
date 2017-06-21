db97bf3b3e64da86ab2092c6dd7a37c974d5b371
Leontiev Aleksey <web-a.leon@ukr.net>
2017-06-21 09:53:45 +0300

    Begin with underscores

---
+++ inc/customizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Molnium Theme Customizer
+ *
+ * @package Molnium
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function molnium_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'molnium_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function molnium_customize_preview_js() {
+	wp_enqueue_script( 'molnium_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'molnium_customize_preview_js' );
